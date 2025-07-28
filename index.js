24ec49785377c10b3e431f4406b51bcf2c80a6f5
sajdeen-dev <sayyedsajdeen.dev@gmail.com>
2025-07-29 00:05:55 +0530

    Data  Added

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 
 const PORT = 3000;
+const data = {"ip":"152.58.97.49","userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36"}
+
+app.get("/api/data", (req, res) => {
+  res.json(data);
+});
+
 
 app.get("/", (req, res) => {
   res.send("Hello, VideoTube!");
@@ -19,3 +25,5 @@ app.get("/login", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+
